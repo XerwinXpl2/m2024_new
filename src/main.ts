49cf5946b80f61d63ac6e72100e36cc4dace908b
E15dev <e15.rgbcubestudio@gmail.com>
2024-10-25 13:57:03 +0200

    fix: add forgotten division in renderer

--- src/main.ts
+++ src/main.ts
@@ -17,20 +17,22 @@ let camera: point = { x: 0, y: 0 };
 
 let TILESIZE: number = 32;
 
-function renderLoop() {
+function renderLoop(ft: number) {
+    camera.x = Math.sin(ft / 1000) * 10;
     let t = performance.now();
     cv.width = window.innerWidth;
     cv.height = window.innerHeight;
-    ctx.fillStyle = "#ffffff";
-    ctx.fillRect(0, 0, cv.width, cv.height);
 
     for (let y = 0; y < cv.height / TILESIZE; y++) {
         let sp = getTileInfo(0, y);
         let ll = 0;
-        for (let x = 0; x < cv.width; x++) {
+        let ti;
+        let x = 0;
+        while (x < cv.width / TILESIZE) {
             let ti = getTileInfo(x, y);
             if (ti == sp) {
                 ll++;
+                x++;
                 continue;
             }
             ctx.fillStyle =
@@ -44,12 +46,28 @@ function renderLoop() {
             ctx.fillRect(
                 (x - ll) * TILESIZE,
                 y * TILESIZE,
-                TILESIZE * (ll+1),
+                TILESIZE * (ll + 1),
                 TILESIZE,
             );
             ll = 0;
             sp = ti;
+            x++;
+            console.log(x);
         }
+        ctx.fillStyle =
+            ti == TileType.cyan
+                ? "cyan"
+                : ti == TileType.key
+                  ? "black"
+                  : ti == TileType.magenta
+                    ? "magenta"
+                    : "yellow";
+        ctx.fillRect(
+            (x - ll) * TILESIZE,
+            y * TILESIZE,
+            TILESIZE * (ll + 1),
+            TILESIZE,
+        );
     }
 
     ctx.font = `${TILESIZE}px "JetBrains mono"`;
@@ -57,4 +75,4 @@ function renderLoop() {
     ctx.fillText(`${1000.0 / (performance.now() - t)} fps`, 0, TILESIZE);
     requestAnimationFrame(renderLoop);
 }
-renderLoop();
+renderLoop(0);

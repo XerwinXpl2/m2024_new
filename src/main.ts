b48ddd415515656c6df65aba09c61224ae75ab35
Erwin Grudowski <xerwinxpl@gmail.com>
2025-01-13 20:35:28 +0100

    chore: clean up and split into multiple files

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { player, players, updatePlayerPosition } from "./players";
+import { mod } from "./utils";
 import { getTileInfo } from "./worldgen/generator";
 import { TileType } from "./worldgen/types";
 
@@ -9,75 +11,15 @@ const ctx = ctxn as CanvasRenderingContext2D;
 const camera: { x: number; y: number } = { x: 0, y: 0 };
 const TILESIZE = 32;
 
-const keys = { up: false, down: false, left: false, right: false };
-
-const players: {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-    speed: number;
-    fc: string;
-}[] = [
-    { x: 0, y: 0, width: TILESIZE, height: TILESIZE, speed: 8, fc: "white" },
-    { x: 100, y: 0, width: TILESIZE, height: TILESIZE, speed: 8, fc: "purple" },
-];
-
-function np() {
-    currentPlayer = (currentPlayer + 1) % players.length;
-}
-
-let currentPlayer: number = 0;
-
-document.addEventListener("keydown", (event) => {
-    if (event.key == "w") keys.up = true;
-    if (event.key == "s") keys.down = true;
-    if (event.key == "a") keys.left = true;
-    if (event.key == "d") keys.right = true;
-    if (event.key == "g") np();
-});
-
-document.addEventListener("keyup", (event) => {
-    if (event.key == "w") keys.up = false;
-    if (event.key == "s") keys.down = false;
-    if (event.key == "a") keys.left = false;
-    if (event.key == "d") keys.right = false;
-});
-
-function updatePlayerPosition() {
-    const player = players[currentPlayer];
-
-    let dx = 0;
-    let dy = 0;
-
-    if (keys.up) dy -= 1;
-    if (keys.down) dy += 1;
-    if (keys.left) dx -= 1;
-    if (keys.right) dx += 1;
-
-    if (dx != 0 && dy != 0) {
-        const length = Math.sqrt(dx * dx + dy * dy);
-        dx /= length;
-        dy /= length;
-    }
-
-    player.x += dx * player.speed;
-    player.y += dy * player.speed;
-
-    camera.x = player.x - cv.width / 2 + player.width / 2;
-    camera.y = player.y - cv.height / 2 + player.height / 2;
-}
-
-function mod(n: number, m: number): number {
-    return ((n % m) + m) % m;
-}
-
 function renderLoop(_: number = 0) {
+    const startTime = performance.now();
+
     cv.width = window.innerWidth;
     cv.height = window.innerHeight;
-    updatePlayerPosition();
 
-    const startTime = performance.now();
+    updatePlayerPosition(player)
+    camera.x = (player.x * TILESIZE) + (player.width * TILESIZE - cv.width)*0.5;
+    camera.y = (player.y * TILESIZE) + (player.height * TILESIZE - cv.height)*0.5;
 
     const newx = Math.floor(camera.x / TILESIZE);
     const newy = Math.floor(camera.y / TILESIZE);
@@ -94,7 +36,7 @@ function renderLoop(_: number = 0) {
             )
                 length++;
 
-            ctx.fillStyle = getTileColor(tile);
+            ctx.fillStyle = TileType[tile];
             ctx.fillRect(
                 Math.floor(x * TILESIZE - mod(camera.x, TILESIZE)),
                 Math.floor(y * TILESIZE - mod(camera.y, TILESIZE)),
@@ -108,36 +50,18 @@ function renderLoop(_: number = 0) {
 
     players.forEach((p) => {
         ctx.fillStyle = p.fc;
-        ctx.fillRect(p.x - camera.x, p.y - camera.y, p.width, p.height);
+        ctx.fillRect(p.x*TILESIZE - camera.x, p.y*TILESIZE - camera.y, p.width*TILESIZE, p.height*TILESIZE);
     });
 
+    // debug overlay
     ctx.font = `${TILESIZE}px monospace`;
-    ctx.fillStyle = "#ffffff";
-    ctx.fillText(
-        `${Math.round(1000.0 / (performance.now() - startTime))} fps`,
-        0,
-        TILESIZE,
-    );
-    ctx.fillText(
-        `${players[currentPlayer].x} ${players[currentPlayer].y}`,
-        0,
-        1 + 2 * TILESIZE,
-    );
+    let tmp: number = (performance.now() - startTime);
+    ctx.fillStyle = tmp > 4 ? "red" : tmp > 2 ? "orange" : "green";
+    ctx.fillText(`${Math.round(tmp)} ms`, 0, TILESIZE);
+    ctx.fillStyle = "black";
+    ctx.fillText(`${player.x} ${player.y}`, 0, 1 + 2 * TILESIZE);
 
     requestAnimationFrame(renderLoop);
-}
-
-function getTileColor(tileType: TileType): string {
-    switch (tileType) {
-        case TileType.cyan:
-            return "cyan";
-        case TileType.key:
-            return "black";
-        case TileType.magenta:
-            return "magenta";
-        default:
-            return "yellow";
-    }
 }
 
 renderLoop();
---
+++ src/players.ts
@@ -0,0 +1,60 @@
+// all numeric units in tiles
+interface Player {
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    speed: number,
+    fc: string,
+}
+
+const players: Player[] = [
+    { x: 0, y: 0, width: 1, height: 1, speed: 0.25, fc: "white" },
+    { x: 2, y: 0, width: 1, height: 1, speed: 0.25, fc: "purple" },
+];
+
+let currentPlayerIndex = 0;
+let player = players[0];
+
+function np(): void {
+    currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
+    player = players[currentPlayerIndex];
+}
+
+const keys = { up: false, down: false, left: false, right: false };
+
+document.addEventListener("keydown", (event) => {
+    if (event.key.toLowerCase() == "w") keys.up = true;
+    if (event.key.toLowerCase() == "s") keys.down = true;
+    if (event.key.toLowerCase() == "a") keys.left = true;
+    if (event.key.toLowerCase() == "d") keys.right = true;
+    if (event.key.toLowerCase() == "g") np();
+});
+
+document.addEventListener("keyup", (event) => {
+    if (event.key.toLowerCase() == "w") keys.up = false;
+    if (event.key.toLowerCase() == "s") keys.down = false;
+    if (event.key.toLowerCase() == "a") keys.left = false;
+    if (event.key.toLowerCase() == "d") keys.right = false;
+});
+
+function updatePlayerPosition(player: Player) {
+    let dx = 0;
+    let dy = 0;
+
+    if (keys.up) dy -= 1;
+    if (keys.down) dy += 1;
+    if (keys.left) dx -= 1;
+    if (keys.right) dx += 1;
+
+    if (dx != 0 && dy != 0) {
+        const length = Math.sqrt(dx * dx + dy * dy);
+        dx /= length;
+        dy /= length;
+    }
+
+    player.x += dx * player.speed;
+    player.y += dy * player.speed;
+}
+
+export { player, players, currentPlayerIndex, updatePlayerPosition};
